4de85c4760f04a7337269f072ffcb83064ac2298
Fernando Serena <kudhmud@gmail.com>
2018-02-20 22:24:00 +0100

    Ensure GraphDB API is ready to accept a new repository

--- discovery/graph/init.py
+++ discovery/graph/init.py
@@ -5,11 +5,16 @@ files = {'config': open('/root/graph/repo-config.ttl', 'rb')}
 
 for i in range(5):
 	try:
-		r = requests.post('http://graph:7200/rest/repositories', files=files)
-		if 300 > r.status_code >= 200:
-			break
+		rg = requests.get('http://graph:7200/rest/repositories')
+		if rg.status_code == 200:
+			r = requests.post('http://graph:7200/rest/repositories', files=files,
+								timeout=2)
+			if 300 > r.status_code >= 200:
+				print 'repository created!'
+				break
 	except Exception as e:
 		print e.message
 	sleep(2)
 
+print 'finished trying to create a repository in graphdb'
 
